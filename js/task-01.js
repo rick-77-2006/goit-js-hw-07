7482eb1585edefff9df099e996f6eb54430c9fd7
Rick <46992687+rick-77-2006@users.noreply.github.com>
2023-12-17 15:12:43 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesCount = document.querySelectorAll("ul#categories li.item");
+console.log(`Number of categories: ${categoriesCount.length}`);
+
+categoriesCount.forEach((category) => {
+  const title = category.querySelector("h2").innerText;
+  const innerCategories = category.querySelectorAll("li").length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${innerCategories}`);
+});
