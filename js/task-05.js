807d7027ea2fa22c7baaa384c4b4195b0355a8f6
Rick <46992687+rick-77-2006@users.noreply.github.com>
2024-01-12 13:30:41 +0200

    цйвыфвыф

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+document.addEventListener("DOMContentLoaded", function () {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")}`;
+  }
+
+  const changeColorButton = document.querySelector(".change-color");
+  const widget = document.querySelector(".widget");
+  const colorSpan = document.querySelector(".color");
+
+  changeColorButton.addEventListener("click", () => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    widget.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+  });
+});
--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const userInput = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesDiv = document.querySelector("#boxes");
+
+createButton.addEventListener("click", () => {
+  const amount = Number(userInput.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    userInput.value = "";
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  boxesDiv.innerHTML = "";
+
+  let initialBoxSize = 30;
+  let boxSizeIncrement = 10;
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${initialBoxSize}px`;
+    box.style.height = `${initialBoxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesDiv.appendChild(box);
+
+    initialBoxSize += boxSizeIncrement;
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
